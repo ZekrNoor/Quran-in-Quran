23fd8933db73dc601930b9796f7c21e7d1cbe078
champnz <amir0ghahremanian@gmail.com>
2025-07-28 20:33:12 +0330

    chore: change project name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:koran/main.dart';
+import 'package:quran_in_quran/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
