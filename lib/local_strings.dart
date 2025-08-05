3b15100a2c261a44ac8e0bd3a9da171f62fcc51f
champnz <amir0ghahremanian@gmail.com>
2025-08-05 22:55:57 +0330

    closes #9: implement home page rough structure

---
+++ lib/local_strings.dart
@@ -0,0 +1,11 @@
+final class LocalStrings {
+  static const String welcome = 'خوش آمدید';
+  static const String hello = 'سلام بر شما';
+  static const String todaysAyah = 'آیه امروز';
+  static const String increment = 'افزایش';
+  static const String dailyMeditation1 = 'مکث';
+  static const String dailyMeditation2 = 'روزانه';
+  static const String events = 'رویداد های نزدیک';
+  static const String resumeReading = 'ادامه مطالعه';
+  static const String quranMenu = 'فهرست';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,354 @@
-import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+
+import 'local_strings.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
+  if (Platform.isWindows) {
+    WindowManager.instance.setMinimumSize(const Size(440, 956));
+    WindowManager.instance.setMaximumSize(const Size(440, 956));
+    WindowManager.instance.setResizable(false);
+  }
+
+  runApp(const QiQApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class QiQApp extends StatelessWidget {
+  const QiQApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: QiQHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class EventsNearYou extends StatelessWidget {
+  const EventsNearYou({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(32)),
+        color: CupertinoColors.systemYellow,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+      child: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 14, vertical: 10),
+        child: Row(
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              LocalStrings.events,
+              style: TextStyle(
+                fontFamily: 'Estedad',
+                fontSize: 12.0,
+                fontVariations: [
+                  FontVariation('wght', 600.0),
+                  FontVariation('KSHD', 100.0),
+                ],
+              ),
             ),
+            Icon(Icons.place_outlined, color: CupertinoColors.black),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Icon(
+          Icons.account_circle_outlined,
+          color: CupertinoColors.black,
+          size: 32,
+        ),
+        Padding(
+          padding: EdgeInsetsGeometry.symmetric(horizontal: 4),
+          child: EventsNearYou(),
+        ),
+        const Spacer(),
+        // TODO: implement menu animation & functionality
+        Icon(Icons.menu, color: CupertinoColors.black, size: 32),
+        // IconButton(onPressed: onPressed, icon: icon)
+      ],
+    );
+  }
+}
+
+class TodaysAyah extends StatelessWidget {
+  const TodaysAyah({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          // opacity: 0.18,
+          // scale: 1.0,
+          fit: BoxFit.cover,
+          image: AssetImage('assets/images/desert.jpg'),
+        ),
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.all(Radius.circular(32)),
+      ),
+      child: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+            Text(
+              LocalStrings.todaysAyah,
+              style: TextStyle(
+                color: CupertinoColors.white,
+                fontFamily: 'Estedad',
+                // fontWeight: FontWeight.w100,
+                fontSize: 16,
+                fontVariations: [
+                  FontVariation('wght', 400.0),
+                  FontVariation('KSHD', 180.0),
+                ],
+              ),
+            ),
+            SizedBox(height: 200),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AlQuranAlKarim extends StatelessWidget {
+  const AlQuranAlKarim({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          opacity: 0.18,
+          scale: 1.0,
+          fit: BoxFit.none,
+          image: AssetImage('assets/images/al_quran_al_karim.png'),
+        ),
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.all(Radius.circular(44)),
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(255, 255, 240, 180),
+            blurRadius: 28,
+            spreadRadius: 0,
+          ),
+          BoxShadow(
+            color: const Color.fromARGB(16, 0, 0, 0),
+            blurRadius: 28,
+            spreadRadius: 0,
+          ),
+        ],
+        color: CupertinoColors.white,
+      ),
+      child: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsetsGeometry.symmetric(vertical: 20),
+              child: Image.asset(
+                'assets/images/al_quran_al_karim.png',
+                width: 203.0,
+                height: 170.52,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color.fromARGB(255, 119, 119, 119),
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: CupertinoButton.filled(
+                    padding: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                    borderRadius: BorderRadius.circular(100),
+                    color: Color.fromARGB(255, 244, 244, 246),
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        const Text(
+                          LocalStrings.quranMenu,
+                          style: TextStyle(
+                            color: CupertinoColors.black,
+                            fontFamily: 'Estedad',
+                            fontSize: 12.0,
+                            fontVariations: [
+                              FontVariation('wght', 500.0),
+                              FontVariation('KSHD', 100.0),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(Icons.menu, color: CupertinoColors.black),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color.fromARGB(255, 196, 155, 0),
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: CupertinoButton.filled(
+                    padding: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+                    borderRadius: BorderRadius.circular(100),
+                    color: Color.fromARGB(255, 255, 219, 79),
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        const Text(
+                          LocalStrings.resumeReading,
+                          style: TextStyle(
+                            color: CupertinoColors.black,
+                            fontFamily: 'Estedad',
+                            fontSize: 12.0,
+                            fontVariations: [
+                              FontVariation('wght', 500.0),
+                              FontVariation('KSHD', 100.0),
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(Icons.play_arrow, color: CupertinoColors.black),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DailyMeditation extends StatelessWidget {
+  const DailyMeditation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: BorderDirectional(
+          bottom: BorderSide(color: CupertinoColors.systemYellow, width: 4),
+        ),
+      ),
+      child: Row(
+        children: [
+          const Text(
+            LocalStrings.dailyMeditation2,
+            style: TextStyle(
+              fontFamily: 'Estedad',
+              fontSize: 24.0,
+              fontVariations: [
+                FontVariation('wght', 600.0),
+                FontVariation('KSHD', 200.0),
+              ],
+            ),
+          ),
+          SizedBox(width: 6),
+          const Text(
+            LocalStrings.dailyMeditation1,
+            style: TextStyle(
+              fontFamily: 'Estedad',
+              fontSize: 24.0,
+              fontVariations: [
+                FontVariation('wght', 600.0),
+                FontVariation('KSHD', 100.0),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class QiQHome extends StatelessWidget {
+  const QiQHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      // navigationBar: const CupertinoNavigationBar.large(
+      //   largeTitle: Text(
+      //     LocalStrings.welcome,
+      //     style: TextStyle(
+      //       fontFamily: 'Estedad',
+      //       fontSize: 42.0,
+      //       fontVariations: [
+      //         FontVariation('wght', 800.0),
+      //         FontVariation('KSHD', 100.0),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      child: SafeArea(
+        child: ScrollConfiguration(
+          behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+          child: ListView(
+            physics: ClampingScrollPhysics(),
+            children: [
+              Padding(
+                padding: EdgeInsetsGeometry.symmetric(
+                  horizontal: 12,
+                  vertical: 20,
+                ),
+                child: HomeHeader(),
+              ),
+              SizedBox(height: 40),
+              Padding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
+                child: AlQuranAlKarim(),
+              ),
+              SizedBox(height: 40),
+              Padding(
+                padding: EdgeInsetsGeometry.only(right: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [const Spacer(), DailyMeditation()],
+                ),
+              ),
+              SizedBox(height: 40),
+              Padding(
+                padding: EdgeInsetsGeometry.symmetric(horizontal: 12),
+                child: TodaysAyah(),
+              ),
+              SizedBox(height: 40),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
